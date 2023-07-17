311f94e08f8325965a95f70e6e0114430771e3f2
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-07-17 19:04:10 +0200

    test page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Nuxt Ionic</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Nuxt Ionic</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div id="container">
+        <h1>Nuxt Ionic Module</h1>
+        <ion-button router-link="/resources">Get Started</ion-button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
