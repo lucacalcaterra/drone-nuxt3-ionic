b4681579637c151e4829207c3ecedfd57c0caabd
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-07-17 20:31:07 +0200

    stuck

---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-content ref="content">
+    <ion-button @click="geoPosition">Get Location</ion-button>
+    <ion-input ref="currentLocation" type="text" placeholder="Title"></ion-input>
+
+  </ion-content>
+</template>
+
+<script setup lang="ts">
+import { IonButton, IonContent } from "@ionic/vue";
+import { Geolocation } from "@capacitor/geolocation";
+
+import { ref } from "vue";
+
+const content = ref();
+const currentLocation = ref('');
+const geoPosition = async () => {
+  const coordinates = await Geolocation.getCurrentPosition();
+  currentLocation= "ciao";
+
+  console.log("Current position:", coordinates.timestamp);
+};
+</script>
